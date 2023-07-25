a7325527f3a69228de490976d172fd563c2adf01
bekkouch <bekkouch@outlook.com>
2023-07-25 12:54:21 +0200

    creation des composants de bases

--- terraformer/src/app/app.module.ts
+++ terraformer/src/app/app.module.ts
@@ -5,19 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
 import { HomeComponent } from './home/home.component';
+import { PromptComponent } from './prompt/prompt.component';
+import { PromptModule } from './prompt/prompt.module';
+import { ScriptComponent } from './script/script.component';
+import { ScriptModule } from './script/script.module';
+import { SelectorComponent } from './selector/selector.component';
+import { SelectorModule } from './selector/selector.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ToolbarModule } from './toolbar/toolbar.module';
+import { FooterComponent } from './footer/footer.component';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PromptComponent,
+    ScriptComponent,
+    SelectorComponent,
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PromptModule,
+    ScriptModule,
+    SelectorModule,
+    ToolbarModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- terraformer/src/app/home/home.module.ts
+++ terraformer/src/app/home/home.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
   ],
   imports: [
     CommonModule,
